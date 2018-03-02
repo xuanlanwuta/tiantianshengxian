40d08ed95901f25b3e21a4449eb60c19698e2dca
xuanlanwuta <zuiaichuju@126.com>
2018-03-02 10:29:03 +0800

    详情页完成

--- dailyfresh/apps/goods/admin.py
+++ dailyfresh/apps/goods/admin.py
@@ -1,10 +1,44 @@
 from django.contrib import admin
-from goods.models import GoodsCategory, Goods, GoodsSKU
+from goods.models import GoodsCategory, Goods, GoodsSKU, IndexPromotionBanner
+from celery_tasks.tasks import generate_static_index_html
+from django.core.cache import cache
 
 
 # Register your models here.
 
 
+class BaseAdmin(admin.ModelAdmin):
+    """模型管理类的基类"""
+
+    def save_model(self, request, obj, form, change):
+        """保存数据时调用"""
+
+        # 保存数据
+        obj.save()
+        # 异步生成静态页面
+        generate_static_index_html.delay()
+        # 清除缓存
+        cache.delete('index_page_data')
+
+
+    def delete_model(self, request, obj):
+        """删除数据时调用的"""
+
+        # 删除数据
+        obj.delete()
+        # 异步生成静态页面
+        generate_static_index_html.delay()
+        # 清除缓存
+        cache.delete('index_page_data')
+
+
+class IndexPromotionBannerAdmin(BaseAdmin):
+    """主页商品活动模型类的管理类"""
+
+    pass
+
+
 admin.site.register(GoodsCategory)
 admin.site.register(Goods)
 admin.site.register(GoodsSKU)
+admin.site.register(IndexPromotionBanner, IndexPromotionBannerAdmin)
--- dailyfresh/celery_tasks/tasks.py
+++ dailyfresh/celery_tasks/tasks.py
@@ -2,7 +2,8 @@
 from celery import Celery
 from django.core.mail import send_mail
 from django.conf import settings
-
+from goods.models import GoodsCategory, IndexPromotionBanner, IndexGoodsBanner, IndexCategoryGoodsBanner
+from django.template import loader
 # 创建celery应用对象
 app = Celery ('celery_tasks.tasks' , broker='redis://192.168.0.22:6379/4')
 
@@ -19,3 +20,50 @@ def send_active_email(to_email , user_name , token):
                 '<br/><p>请点击此链接激活您的帐号<a href="http://127.0.0.1:8000/users/active/%s">' \
                 'http://127.0.0.1:8000/users/active/%s</a></p>' % (user_name , token , token)
     send_mail (subject , body , sender , receiver , html_message=html_body)
+
+
+@app.task
+def generate_static_index_html():
+    """生成静态主页"""
+
+    # 查询商品分类信息
+    categorys = GoodsCategory.objects.all()
+
+    # 查询图片轮播信息
+    index_banners = IndexGoodsBanner.objects.all().order_by('index')
+
+    # 查询商品活动信息
+    promotion_banners = IndexPromotionBanner.objects.all().order_by('index')
+
+    # 查询商品分类列表信息
+    for category in categorys:
+        # 查询标题类型展示的商品
+        title_banners = IndexCategoryGoodsBanner.objects.filter(category=category, display_type=0)
+        category.title_banners = title_banners
+
+        # 查询图片类型展示的商品
+        image_banners = IndexCategoryGoodsBanner.objects.filter(category=category, display_type=1)
+        category.image_banners = image_banners
+
+    # 查询购物车信息
+    cart_num = 0
+
+    # 构造上下文
+    context = {
+        'categorys': categorys,
+        'index_banners': index_banners,
+        'promotion_banners': promotion_banners,
+        'cart_num': cart_num
+    }
+
+    # 加载模板
+    print('static_index')
+    template = loader.get_template('static_index.html')
+    # 上下文渲染模板，得到html数据
+    html_data = template.render(context)
+
+    # 获取静态文件路径
+    file_path = os.path.join(settings.STATICFILES_DIRS[0], 'index.html')
+    # 将生成的html数据，存储到静态文件夹
+    with open(file_path, 'w') as file:
+        file.write(html_data)
